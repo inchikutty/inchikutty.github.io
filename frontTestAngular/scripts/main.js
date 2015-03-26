5fe5cb14bf720d2f2e8b989af9e79a256ec34725
inchikutty <inchikutty13@gmail.com>
2015-03-26 21:54:27 +0530

    updated Angularjs mockup

--- frontTestAngular/scripts/main.js
+++ frontTestAngular/scripts/main.js
@@ -1,7 +1,55 @@
-var phonecatApp = angular.module('patchApp', []);   
-phonecatApp.controller('PatchListCtrl', function ($scope, $http) {
- $http.get('data/patch.json').success(function(data) {
-$scope.phones = data;
-}); 
-$scope.orderProp = 'age';
+var app = angular.module('patchApp', []);
+app.controller('patchCtrl', function ($scope, $http) {
+	$scope.patches = [];
+	$http.get('data/patch.json').success(function(data) {
+		$scope.patches = data;
+	});
+	$scope.state= 'pending';
+	$scope.metric = function( id ){
+		/*switch( id ){
+			case: 
+			break;
+		}*/
+		return '60';
+	}
+	$scope.build = function( id ){
+		/*switch( id ){
+			case: 
+			break;
+		}*/
+	}
+	$scope.unit = function( id ){
+		/*switch( id ){
+			case: 
+			break;
+		}*/
+	}
+	$scope.func = function( id ){
+		/*switch( id ){
+			case: 
+			break;
+		}*/
+	}
+	$scope.outcome = function( id){
+		/*switch( id ){
+			case: 
+			break;
+		}*/
+	}
 });
+/*
+app.directive('patchUpdate', ['$parse', function($parse) {
+    return {
+        restrict: "A",
+        link: function(scope, element, attrs) {
+            parsed = $parse(attrs.find);
+            if($(element).find( 'id')==scope.patches.id){
+				$(element).find('#state').on({
+				
+				})
+             
+            }
+        }
+    }
+}]);*/
+
